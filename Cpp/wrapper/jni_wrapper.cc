23a4f669cc92c3de18c7e1e1c6337ef8ef9d5c7c
KirillAldashkin <a89086520092k@yandex.ru>
2024-12-22 01:03:43 +0300

    Fixed formatting

--- Cpp/wrapper/jni_wrapper.cc
+++ Cpp/wrapper/jni_wrapper.cc
@@ -31,10 +31,10 @@ static jboolean GetIsPrime(JNIEnv* env, jobject obj, jlong sieve, jlong at) {
 
 // Java export table
 static JNINativeMethod exports[] = {
-	{ "CreateSieve", "(J)J", reinterpret_cast<void*>(&CreateSieve) },
-	{ "DestroySieve", "(J)V", reinterpret_cast<void*>(&DestroySieve) },
-	{ "GetSize", "(J)J", reinterpret_cast<void*>(&GetSize) },
-	{ "GetIsPrime", "(JJ)Z", reinterpret_cast<void*>(&GetIsPrime) }
+  { "CreateSieve", "(J)J", reinterpret_cast<void*>(&CreateSieve) },
+  { "DestroySieve", "(J)V", reinterpret_cast<void*>(&DestroySieve) },
+  { "GetSize", "(J)J", reinterpret_cast<void*>(&GetSize) },
+  { "GetIsPrime", "(JJ)Z", reinterpret_cast<void*>(&GetIsPrime) }
 };
 
 // Class where 'native' functions are defined
@@ -42,34 +42,34 @@ constexpr const char* kJavaClassName = "Sieve";
 
 // Java Runtime helpers
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-	JNIEnv* env;
+  JNIEnv* env;
 
   // 1. Resolve JVM interface
-	if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_8) != JNI_OK)
-		return -1;
+  if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_8) != JNI_OK)
+    return -1;
 
   // 2. Find Java side of this library
-	jclass clazz = env->FindClass(kJavaClassName);
-	if (clazz == nullptr) return -1;
+  jclass clazz = env->FindClass(kJavaClassName);
+  if (clazz == nullptr) return -1;
 
   // 3. Bind our impletementations to that class
-	jint res = env->RegisterNatives(clazz, exports, sizeof(exports) / sizeof(*exports));
-	if (res != 0) return -1;
+  jint res = env->RegisterNatives(clazz, exports, sizeof(exports) / sizeof(*exports));
+  if (res != 0) return -1;
 
-	return JNI_VERSION_1_8;
+  return JNI_VERSION_1_8;
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved) {
-	JNIEnv* env;
+  JNIEnv* env;
 
   // 1. Resolve JVM interface
-	if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_8) != JNI_OK)
-		return;
+  if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_8) != JNI_OK)
+    return;
  
   // 2. Find Java side of this library
-	jclass clazz = env->FindClass(kJavaClassName);
-	if (clazz == nullptr) return;
+  jclass clazz = env->FindClass(kJavaClassName);
+  if (clazz == nullptr) return;
 
   // 3. Unbind our impletementations from that class
-	env->UnregisterNatives(clazz);
+  env->UnregisterNatives(clazz);
 }
